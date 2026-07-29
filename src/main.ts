090c44dac19d65c4810f8e48d19fe26bc80968f3
liuchang <sorry00asd@gmail.com>
2026-07-29 18:00:00 +0800

    feat: 安装nest相关依赖

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  await app.listen(3000);
+  console.log("Server running on http://localhost:3000");
+}
+
+bootstrap();
